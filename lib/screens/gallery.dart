c1557c7a90107a5b89f1bda91644fd543c4d4925
PrinceSanjivy <sanjivy.android@gmail.com>
2021-05-05 23:03:45 +0530

    gallery completed

--- lib/screens/gallery.dart
+++ lib/screens/gallery.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:repostaffs/components/my_appbar.dart';
+import 'package:repostaffs/components/my_text.dart';
+import 'package:repostaffs/constants.dart';
 
 class Gallery extends StatefulWidget {
   @override
@@ -13,20 +15,36 @@ class _GalleryState extends State<Gallery> {
     return Scaffold(
       appBar: MyAppBar("Gallery"),
       body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection("gallery").snapshots(),
+          stream: FirebaseFirestore.instance
+              .collection("gallery")
+              .orderBy("customerName")
+              .snapshots(),
           builder: (context, gallerySnapshot) {
             if (!gallerySnapshot.hasData)
               return Center(child: CircularProgressIndicator());
 
             return GridView.builder(
+              padding: EdgeInsets.all(8),
               itemCount: gallerySnapshot.data.size,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 15.0,
-                  crossAxisSpacing: 3.0),
+                crossAxisCount: 2,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+              ),
               itemBuilder: (context, index) {
-                return Image.network(
-                    gallerySnapshot.data.docs[index].get("url"));
+                return GridTile(
+                  child: Image.network(
+                    gallerySnapshot.data.docs[index].get("url"),
+                    fit: BoxFit.cover,
+                  ),
+                  footer: Center(
+                    child: MyText(
+                      gallerySnapshot.data.docs[index].get("customerName"),
+                      color: WHITE,
+                      fontWeight: "Medium",
+                    ),
+                  ),
+                );
               },
             );
           }),
